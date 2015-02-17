8b263ab3be7c6571d30813ad0853cc28f0506538
jvalencia <jvalencia@vaduinc.com>
2015-02-16 23:14:11 -0500

    comment for MIN_CLS_IDX constant

--- src/edu/harvard/cscie99/clustering/algorithm/LeaderAlgorithm.java
+++ src/edu/harvard/cscie99/clustering/algorithm/LeaderAlgorithm.java
@@ -28,7 +28,7 @@ public abstract class LeaderAlgorithm<E extends Object> implements IClusterAlgo<
 //	final static String MIN = "MIN";
 //	final static String MIN_CLS_IDX = "MIN_CLS_IDX";
 	final static int MIN = 0; //array position where the min. distance is stored.
-	final static int MIN_CLS_IDX = 1;
+	final static int MIN_CLS_IDX = 1;  //array position where the Cluster ID with the min. distance is stored.
 	
 	public abstract List<String> getRowLabels(E data,	Map<String, Object> clusterParams);
 	public abstract Number getDistance(E data, List<String> rowKeys , int fromIdx, int toIdx);
